89df3d9602752e31b38a5f9924d70ab5217b0631
dizzysch <victorwilbert@gmail.com>
2023-11-10 18:28:53 -0300

    feat: implementando autenticação e autorização

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog
+{ 
+    public class Configuration
+    {
+        public static string JwtKey = "98SHDKJLFNfdrs98dfdsajf9sdfs09fd7A9YFDS9GF23LRH3E9F8DFSHD9FDFN99ddfnsdf";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_rs98dfdsa==";
+    }
+}
